c026d4a2aa786e304f802af99abd3ccfe9538a28
MaxMillington <maxmillington89@gmail.com>
2015-11-30 11:45:57 -0500

    add iteration 4, editing links, including tests

---
+++ spec/features/user_edits_link_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+describe 'user edits a link' do
+  before do
+    steve = User.create(email: 'steve@steve.com',
+                password: 'password',
+                password_confirmation: 'password')
+
+    steve.links.create(title: 'Wildwood', url: 'http://www.wildwoodguitars.com/')
+
+    visit root_path
+    click_link 'Login'
+    fill_in 'Email', with: 'steve@steve.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
+  end
+
+  context 'with valid data' do
+    it 'works' do
+      click_link 'edit'
+      fill_in 'link_title', with: 'Wildwood Amps'
+      fill_in 'link_url', with: 'http://www.wildwoodguitars.com/amps/index.htm'
+      click_button 'Submit'
+
+      expect(current_path).to eq('/links')
+      expect(page).to have_content('Wildwood Amps')
+      expect(page).to have_content('http://www.wildwoodguitars.com/amps/index.htm')
+    end
+  end
+
+  context 'with invalid data' do
+    it 'does not work' do
+      click_link 'edit'
+      fill_in 'link_title', with: 'dumb'
+      fill_in 'link_url', with: 'dumb'
+      click_button 'Submit'
+      
+      expect(page).to have_content('You must enter a title and valid url')
+    end
+  end
+end
