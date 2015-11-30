5b24767ddf872e30f77aa0cdf11220f9edb78d48
MaxMillington <maxmillington89@gmail.com>
2015-11-30 09:20:00 -0500

    add basic routes, controllers, and database migrations based on project specs

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,27 @@
+class LinksController < ApplicationController
+
+  def new
+
+  end
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def edit
+
+  end
+
+end
